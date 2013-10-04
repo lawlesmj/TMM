b277720fcbbe5dc88756183738eee6ed6ef6168f
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-10-04 12:46:08 -0400

    Create A_L-matrixMult.c

---
+++ A_L-matrixMult.c
@@ -0,0 +1,24 @@
+// includes
+
+//Structures
+//Matrix structure
+typedef struct {
+    uint rows;
+    uint cols;
+    int *array;
+} matrix_t;
+
+//thread param structure
+typedef struct {
+    uint row;
+    uint col;
+} pos_t;
+
+//thread function prototype
+void *matMult( void *param );
+
+int main(int argc, string argv[]) {
+}
+
+void *matMult( void *param ) {
+}
