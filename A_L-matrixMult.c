7b2da4a3cdb07a4c883f4106c51ea9777c0a8cda
applegde <applegde@dukes.jmu.edu>
2013-10-07 19:25:01 -0400

    Update A_L-matrixMult.c

--- A_L-matrixMult.c
+++ A_L-matrixMult.c
@@ -18,7 +18,7 @@ Submitted on: ?
 #include <stdio.h>          /* lets me play with files */
 #include <stdlib.h>         /* adds useful system functions */
 #include <pthread.h>        /* lets us play with threads */
-
+                            //will need to complid with -lpthread
 //Structures
 //Matrix structure
 typedef struct {
@@ -37,14 +37,25 @@ typedef struct {
 void *matMult( void *param );
 
 int main(int argc, string argv[]) {
+    matrix_t * matrix1;
+    matrix_t * matrix2;
+    matIntake(&matrix1, &matrix2);
     
 }
 
 void *matMult( void *param ) {
 }
 
-void matIntake(){
-    //create matrix struct
+void matIntake(matrix_t * firstMat, matrix_t * secMat, String fileName){
+    
+    FILE *file = fopen(fileName, "r"); //say r as the second param because we only look to read the file
+    
+    if( file == NULL)
+    {
+        printf("There is no file by the name %s Closing program", fileName);
+        return 1;
+    }
+   
     //loop1 while there is still info in the file
         //read first two numbers i and j
         //loop2 for i 
