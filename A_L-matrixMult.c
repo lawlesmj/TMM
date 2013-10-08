1a89229f91f3abb9608d1eb9d3c159894ede724f
lawlesmj <lawlesmj@dukes.jmu.edu>
2013-10-08 13:59:08 -0400

    Update A_L-matrixMult.c

--- A_L-matrixMult.c
+++ A_L-matrixMult.c
@@ -19,6 +19,10 @@ Submitted on: ?
 #include <stdlib.h>         /* adds useful system functions */
 #include <pthread.h>        /* lets us play with threads */
                             //will need to complid with -lpthread
+                            
+//A macro to emulate a 2d array. Works just like matrix[row][col].
+#define MATRIX(matrix, row, col) matrix->array[row + col * matrix->rows]
+
 //Structures
 //Matrix structure
 typedef struct {
@@ -36,7 +40,7 @@ typedef struct {
 //thread function prototype
 void *matMult( void *param );
 //File read function prototype
-void matIntake(matrix_t * firstMat, matrix_t * secMat, String fileName);
+void matIntake(matrix_t * firstMat, matrix_t * secMat, char * fileName);
 
 int main(int argc, string argv[]) {
     matrix_t * matrix1;
@@ -48,8 +52,8 @@ int main(int argc, string argv[]) {
 void *matMult( void *param ) {
 }
 
-void matIntake(matrix_t * firstMat, matrix_t * secMat, String fileName){
-    
+void matIntake(matrix_t * firstMat, matrix_t * secMat, char * fileName){
+    unsigned int i,j;
     FILE *file = fopen(fileName, "r"); //say r as the second param because we only look to read the file
     
     if( file == NULL)
@@ -58,16 +62,29 @@ void matIntake(matrix_t * firstMat, matrix_t * secMat, String fileName){
         return 1;
     }
    
-    //read first two numbers i and j for first matrix
-        //loop1 for i 
-            //loop2 for j
-                //store stuff in matrix struct
-    //end loop 1
+    //read first two numbers rows and cols for first matrix
+    firstMat->rows = fscanf(file, "u");
+    firtMat->cols = fscanf(file, "u");
     
-    //read two numbers i and j for second matrix
-        //loop1 for i 
-            //loop2 for j
-                //store stuff in matrix struct
+    //create array for matrix data
+    firstMat->array = (int*) malloc(sizeof(int) * firstMat->rows * firstMat->cols);
+    
+    if(firstMat->array == NULL) exit(1); //bad shit happened
+    
+    //loop1 for rows
+    for(i=0; i < firstMat->rows; i++) {
+        //loop2 for cols
+        for(j=0; j < firstMat->cols; j++) {
+            //store stuff in matrix struct
+            MATRIX(firstMat, i, j) = fscanf(file, "i");
+            
+        }
+    }
+    
+    //read two numbers rows and cols for second matrix
+    //loop1 for rows
+        //loop2 for cols
+            //store stuff in matrix struct
     //end loop 1
     
     //read as ints
